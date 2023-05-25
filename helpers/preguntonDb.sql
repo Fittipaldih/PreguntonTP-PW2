01fe88f2196fe01658080f8f30a5013db23a8d00
Mariana Aquino <maquino501@alumno.unlam.edu.ar>
2023-05-25 13:45:16 -0300

    bd actualizada

--- helpers/preguntonDb.sql
+++ helpers/preguntonDb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-05-2023 a las 01:59:11
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 25-05-2023 a las 09:54:50
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `pregunton`
+-- Base de datos: `preguntondb`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `rol` (
   `Id` int(11) NOT NULL,
   `descripcion` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `rol`
@@ -59,15 +59,18 @@ CREATE TABLE `usuario` (
   `Nombre_usuario` varchar(30) NOT NULL,
   `Foto_perfil` varchar(30) NOT NULL,
   `Id_rol` int(11) NOT NULL,
-  `Hash` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Hash` varchar(255) NOT NULL,
+  `contrasenia_hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`Id`, `Nombre_completo`, `Anio_nacimiento`, `Genero`, `Pais`, `Ciudad`, `Mail`, `Nombre_usuario`, `Foto_perfil`, `Id_rol`, `Hash`) VALUES
-(1, '', '0000-00-00', '', '', '', '', '', '', 0, 0);
+INSERT INTO `usuario` (`Id`, `Nombre_completo`, `Anio_nacimiento`, `Genero`, `Pais`, `Ciudad`, `Mail`, `Nombre_usuario`, `Foto_perfil`, `Id_rol`, `Hash`, `contrasenia_hash`) VALUES
+(1, '', '0000-00-00', '', '', '', '', '', '', 0, '0', ''),
+(2, 'Lisandro Menu', '2003-01-28', 'm', 'Argentina', 'Santa Rosa', 'lisandro@pregunton.com', 'lisandroo', '', 1, '0', ''),
+(4, 'Lisandro', '2003-01-28', 'Masculino', 'Argentina', 'Santa Rosa', 'lisandro@hotmail.com', 'lisandrooo', 'charizard_f.gif', 0, '$2y$10$2/tnSK.F1bhtToekOFeaZe2QcyMjqM/ES5bKZeKUVnpHmYXtEYMNe', '$2y$10$AslBi7ABmHcaq6R5wEgauOFn/a7SOlEoyTf.uMcZQL5dDArFWQPuK');
 
 --
 -- Índices para tablas volcadas
@@ -99,7 +102,7 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
