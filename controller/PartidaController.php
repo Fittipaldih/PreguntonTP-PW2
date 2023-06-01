27a87ebcb5aabe9912a949e051321c82e3abbfb4
Fittipaldih <fittipaldi.h@gmail.com>
2023-06-01 13:18:38 -0300

    Partida ya funciona con preguntas random

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -6,14 +6,12 @@ class PartidaController
     private $renderer;
     private $sessionManager;
     private $questionData;
-    private $answerCorrects;
 
     public function __construct($model, $renderer, $sessionManager)
     {
         $this->partidaModel = $model;
         $this->renderer = $renderer;
         $this->sessionManager = $sessionManager;
-        $this->answerCorrects = 0;
     }
 
     public function home()
@@ -31,7 +29,7 @@ class PartidaController
         return $this->sessionManager->get("isConnected");
     }
 
-    private function renderView()
+    private function renderView($userCorrects = 0)
     {
         /* Al hacer [0] en lugar de pasar el array completo..
            Pasa directamente la descripción o lo que fuese como una cadena de texto
@@ -39,40 +37,39 @@ class PartidaController
            Porque nuestro metodo query devuelve los resultados como un array siempre
         */
         $data['userLogged'] = $this->sessionManager->get("user");
-        $data['answerCorrects'] = $this->answerCorrects;
-        $data+= $this->renderAnswerAndQuestion();
+        $data += $this->renderAnswerAndQuestion($userCorrects);
         $this->renderer->render("Partida", $data);
     }
 
-    private function renderAnswerAndQuestion()
+    private function renderAnswerAndQuestion($userCorrects)
     {
         $question = $this->partidaModel->getQuestion();
-        $answer= $this->partidaModel->getAnswers($question[0]['id_respuesta']);
+        $answer = $this->partidaModel->getAnswers($question[0]['id_respuesta']);
         $this->questionData = [
             'question' => $question[0]['descripcion'],
             'opcionA' => $answer[0]['opcionA'],
             'opcionB' => $answer[0]['opcionB'],
             'opcionC' => $answer[0]['opcionC'],
             'opcionD' => $answer[0]['opcionD'],
-            'correct' => $answer[0]['resp_correcta']
-
+            'answerCorrect' => $answer[0]['resp_correcta'],
+            'userCorrects' => $userCorrects
         ];
         return $this->questionData;
     }
-/*
-    private function checkAnswer()
-    {
-        if (isset($_POST['option'])) {
-            $optionSelected = $_POST['option'];
-            if (isset($this->questionData['correct'])) {
-            if( (string)$this->questionData['correct'] === (string)$optionSelected){
-                $this->answerCorrects+=1;
 
-            }else {
-                var_dump($this->questionData['correct']);
-                var_dump($optionSelected);
-            }
-            }
+    public function checkAnswer()
+    {
+        if (isset($_POST['optionSelected'])) {
+            $optionSelected = $_POST['optionSelected'];
+            $optionCorrect = $_POST['answerCorrect'];
+            $userCorrects = $_POST['userCorrects'];
+                if ($optionSelected === $optionCorrect) {
+                    $userCorrects += 1;
+                }
+                else {
+                    echo 'perdiste';
+                }
+            $this->renderView($userCorrects);
         }
-    }*/
+    }
 }
