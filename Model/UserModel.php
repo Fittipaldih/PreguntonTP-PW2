1bc4c73875b5f5bbdda2618bb6749b1f0d46528b
Fittipaldih <fittipaldi.h@gmail.com>
2023-06-04 17:39:17 -0300

    avances en editar user

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -19,4 +19,35 @@ class UserModel
         return $this->database->query("SELECT * FROM partida WHERE id_usuario =
                         (SELECT Id FROM usuario WHERE Nombre_usuario = '$userName')");
     }
+
+    public function setNameComplete($userLogged, $new)
+    {
+        return $this->database->update("UPDATE usuario SET Nombre_completo='$new' WHERE Nombre_usuario = '$userLogged'");
+    }
+
+    public function setPhoto($userLogged, $new)
+    {
+        return $this->database->update("UPDATE usuario SET Foto_perfil='$new' WHERE Nombre_usuario = '$userLogged'");
+    }
+
+    public function setSex($userLogged, $new)
+    {
+        return $this->database->update("UPDATE usuario SET Genero='$new' WHERE Nombre_usuario = '$userLogged'");
+    }
+
+    public function setBirthDate($userLogged, $new)
+    {
+        return $this->database->update("UPDATE usuario SET Fecha_nacimiento='$new' WHERE Nombre_usuario = '$userLogged'");
+    }
+
+    public function setUbication($userLogged, $new)
+    {
+        return $this->database->update("UPDATE usuario SET Ciudad='$new' WHERE Nombre_usuario = '$userLogged'");
+    }
+
+    public function setMail($userLogged, $new)
+    {
+        return $this->database->update("UPDATE usuario SET Mail='$new' WHERE Nombre_usuario = '$userLogged'");
+    }
+
 }
--- controller/UserController.php
+++ controller/UserController.php
@@ -30,18 +30,17 @@ class UserController
 
     public function renderView()
     {
-        if (isset($_GET['name'])){
-        $userName = $_GET['name']; // esto lo recibe de la view ranking Linea 18
-        $userLogged = $this->getNameUserBySession();
-        $canEdit = ($userName === $userLogged) ? true : false;
-        $data["canEdit"] = $canEdit;
-        $data["user"] = $this->getUserByName($userName);
-        $data["games"] = $this->getUserGamesByName($userName);
-        $data['userLogged'] = $userLogged;
+        if (isset($_GET['name'])) {
+            $userName = $_GET['name']; // esto lo recibe de la view ranking Linea 18
+            $userLogged = $this->getNameUserBySession();
+            $canEdit = ($userName === $userLogged) ? true : false;
+            $data["canEdit"] = $canEdit;
+            $data["user"] = $this->getUserByName($userName);
+            $data["games"] = $this->getUserGamesByName($userName);
+            $data['userLogged'] = $userLogged;
 
-        $this->renderer->render("user", $data);
-        }
-        else {
+            $this->renderer->render("user", $data);
+        } else {
             header("Location: /");
             exit();
         }
@@ -62,39 +61,38 @@ class UserController
         return $this->userModel->getUserByName($name);
     }
 
-    private function edit()
+    public function edit()
     {
-        $userName = $_GET['name'];
-        $userLogged = $this->getNameUserBySession();
-        if ($userLogged === $userName) {
-            $edit = $_GET['edit'];
-            $new = $_GET['new'];
-            switch ($edit) {
-                case 'userName':
-                    $this->userModel->setUserName($userLogged, $new);
-                    break;
-                case 'photo':
-                    $this->userModel->setPhoto($userLogged, $new);
-                    break;
-                case 'nameComplete':
-                    $this->userModel->setNameComplete($userLogged, $new);
-                    break;
-                case 'sex':
-                    $this->userModel->setSex($userLogged, $new);
-                    break;
-                case 'birthDate':
-                    $this->userModel->setBirthDate($userLogged, $new);
-                    break;
-                case 'ubi':
-                    $this->userModel->setUbication($userLogged, $new);
-                    break;
-                case 'mail':
-                    $this->userModel->setMail($userLogged, $new);
-                    break;
-                default :
-                    header("Location: /lobby");
-                    exit();
-            }
+        $edit = $_POST['edit'];
+        $new = $_POST['new'];
+        switch ($edit) {
+            case 'photo':
+                $this->userModel->setPhoto($this->getNameUserBySession(), $new);
+                header("Location: /user");
+                break;
+            case 'nameComplete':
+                $this->userModel->setNameComplete($this->getNameUserBySession(), $new);
+                header("Location: /user");
+                break;
+            case 'genre':
+                $this->userModel->setSex($this->getNameUserBySession(), $new);
+                header("Location: /user");
+                break;
+            case 'birthDate':
+                $this->userModel->setBirthDate($this->getNameUserBySession(), $new);
+                header("Location: /user");
+                break;
+            case 'ubi':
+                $this->userModel->setUbication($this->getNameUserBySession(), $new);
+                header("Location: /user");
+                break;
+            case 'mail':
+                $this->userModel->setMail($this->getNameUserBySession(), $new);
+                header("Location: /user");
+                break;
+            default :
+                header("Location: /lobby");
+                exit();
         }
     }
 }
