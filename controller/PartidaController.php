35102290ec2accf4c2b9e8275faf69aef0494880
Mariana Aquino <maquino501@alumno.unlam.edu.ar>
2023-06-05 20:27:23 -0300

    Ultimo commit de esta rama

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -79,7 +79,6 @@ class PartidaController
                 $this->partidaModel->registerCorrectAnswer($idPregunta, $idUsuario);
                 $this->partidaModel->updateSkillLevel($idPregunta, $idUsuario);
                 $this->renderView($userCorrects);
-
             } else {
                 $this->partidaModel->updateSkillLevel($idPregunta, $idUsuario);
                 $this->partidaModel->insertUserGamesByName($idUsuario, $userCorrects);
