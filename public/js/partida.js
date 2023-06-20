4c4b7e8fc650a0560986830d2cd10b5892c8e716
Fittipaldih <fittipaldi.h@gmail.com>
2023-06-20 19:56:22 -0300

    crear pregunta front

--- public/js/partida.js
+++ public/js/partida.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     cargarAjax();
 });
-
 let progressInterval;
 const progressBar = document.getElementById("myProgressBar");
 const cronometroElement = document.getElementById("cronometro");
@@ -10,75 +9,79 @@ const finalWidth = 100;
 const incrementWidth = (finalWidth / duration) * 100;
 let currentWidth = 0;
 let startTime;
-
 function animateProgressBar() {
     currentWidth += incrementWidth;
     progressBar.style.width = `${currentWidth}%`;
-
     if (!startTime) {
         startTime = Date.now();
     }
-
     const elapsedTime = Date.now() - startTime;
     const remainingTime = duration - elapsedTime;
     const formattedTime = formatTime(remainingTime);
     cronometroElement.textContent = formattedTime;
-
     if (currentWidth >= finalWidth || remainingTime <= 0) {
         clearInterval(progressInterval);
         window.location.href = "/lobby";
     }
 }
-
 function formatTime(time) {
     const seconds = Math.ceil(time / 1000);
     const adjustedSeconds = Math.max(seconds - 1, 0);
     return adjustedSeconds.toString();
 }
-
-function resetProgressBar() {
-    clearInterval(progressInterval);
-    currentWidth = 0;
-    startTime = null;
-    progressInterval = setInterval(animateProgressBar, 100);
-}
-
 function cargarAjax() {
     $.ajax({
         url: 'http://localhost/partida/renderQuestionData',
         method: 'GET',
         dataType: 'json',
         success: function (question) {
-            var categoria = question.id_categoria;
-            if (categoria === '1') {
-                $('#contPartida').css('background-color', '#5fe03e');
-            } else if (categoria === '2') {
-                $('#contPartida').css('background-color', '#e87800');
-            } else if (categoria === '3') {
-                $('#contPartida').css('background-color', '#cf5787');
-            } else if (categoria === '4') {
-                $('#contPartida').css('background-color', '#964396');
-            } else if (categoria === '5') {
-                $('#contPartida').css('background-color', '#f7f358');
-            } else {
-                $('#contPartida').css('background-color', '#B8B0B0');
-            }
-
-            $('#questionCategory').text(question.catDescripcion);
-            $('#questionDescripcion').text(question.descripcion);
-            $('#opciona').text(question.opcionA);
-            $('#opcionb').text(question.opcionB);
-            $('#opcionc').text(question.opcionC);
-            $('#opciond').text(question.opcionD);
-            $('#idQuestionHidden').val(question.id);
             resetProgressBar();
+            setDataQuestion(question);
         },
         error: function (xhr, status, error) {
             console.log(error);
         }
     });
 }
-
+function resetProgressBar() {
+    clearInterval(progressInterval);
+    currentWidth = 0;
+    startTime = null;
+    progressInterval = setInterval(animateProgressBar, 100);
+}
+function setDataQuestion(question) {
+    $('#questionCategory').text(question.catDescripcion);
+    $('#questionDescripcion').text(question.descripcion);
+    $('#opciona').text(question.opcionA);
+    $('#opcionb').text(question.opcionB);
+    $('#opcionc').text(question.opcionC);
+    $('#opciond').text(question.opcionD);
+    $('#idQuestionHidden').val(question.id);
+    setContainerColor(question);
+}
+function setContainerColor(question) {
+    var categoria = question.id_categoria;
+    switch (categoria) {
+        case '1':
+            $('#contPartida').css('background-color', '#58f75d');
+            break;
+        case '2':
+            $('#contPartida').css('background-color', '#f7a058');
+            break;
+        case '3':
+            $('#contPartida').css('background-color', '#f75858');
+            break;
+        case '4':
+            $('#contPartida').css('background-color', '#ad58f7');
+            break;
+        case '5':
+            $('#contPartida').css('background-color', '#f7ef58');
+            break;
+        default:
+            $('#contPartida').css('background-color', '#646464');
+            break;
+    }
+}
 function selected(value) {
     console.log(value);
     $.ajax({
@@ -96,7 +99,6 @@ function selected(value) {
         console.log(errorThrown);
     });
 }
-
 document.addEventListener("DOMContentLoaded", function () {
     var opciona = document.getElementById("opciona");
     var opcionb = document.getElementById("opcionb");
