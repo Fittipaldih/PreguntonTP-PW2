8e96e2ec795feba9ad33517d91120483130ba677
Fittipaldih <fittipaldi.h@gmail.com>
2023-06-23 17:30:40 -0300

    BD Y DER

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -123,4 +123,64 @@ class UserModel
         return $data;
     }
 
+
+    /*
+
+function getCantidadUsuariosPorGenero()
+{
+    $query = "SELECT Genero, COUNT(*) AS cantidad_usuarios
+        FROM usuario
+        GROUP BY Genero";
+
+    return $result;
+}
+
+function getCantidadUsuariosPorGrupoEdad()
+{
+    $query = "SELECT
+        CASE
+        WHEN TIMESTAMPDIFF(YEAR, Fecha_nacimiento, CURDATE()) < 18 THEN 'Menor a 18 años'
+        WHEN TIMESTAMPDIFF(YEAR, Fecha_nacimiento, CURDATE()) BETWEEN 18 AND 64 THEN '18 a 64 años'
+        WHEN TIMESTAMPDIFF(YEAR, Fecha_nacimiento, CURDATE()) >= 65 THEN '65 años en adelante'
+        END AS grupo_edad,
+        COUNT(*) AS cantidad_usuarios
+        FROM usuario
+        GROUP BY grupo_edad";
+
+    return $result;
+}
+
+Agrega opciones de filtro en tu interfaz de usuario: Puedes utilizar elementos como botones, selectores de fechas
+o menús desplegables para permitir al usuario seleccionar el período de tiempo deseado (día, semana, mes o año).
+
+Captura la selección del filtro en tu aplicación: Utiliza JavaScript para capturar la opción seleccionada por el usuario.
+
+Actualiza las consultas SQL con base en el filtro seleccionado:
+Ajusta las consultas SQL para filtrar los datos según el período de tiempo seleccionado.
+Puedes utilizar cláusulas WHERE y funciones de fecha y tiempo en tus consultas para filtrar los datos correctamente.
+
+y agregar un botón de impresión en tu interfaz de usuario
+(gráficos, tablas de datos) listos para imprimir.
+
+
+
+Verificar si ya existe una fecha de inicio de sesión en la sesión
+if (isset($_SESSION['last_login']))
+{
+$lastLogin = $_SESSION['last_login'];
+echo "Última fecha de inicio de sesión: " . $lastLogin;
+}
+
+// Guardar la fecha actual como la última fecha de inicio de sesión
+$_SESSION['last_login'] = date("Y-m-d H:i:s");
+session_start();
+Obtener la última fecha de inicio de sesión
+$lastUserId = $_SESSION['last_user_id'];
+$lastLogin = $_SESSION['last_login'];
+Consulta para contar la cantidad de usuarios creados desde la última fecha de inicio de sesión
+$sql = "SELECT COUNT(*) AS usuarios_nuevos FROM usuario WHERE id > '$lastUserId'";
+echo "Usuarios nuevos desde el último usuario registrado: " . $result['usuarios_nuevos'];
+
+ */
+
 }
